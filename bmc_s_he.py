5a30bcb6a8b99ac8ffb35a49a7ae051bc4f45b7d
M. F. Gu <mfgu@ssl.berkeley.edu>
2018-12-17 22:35:09 -0500

    add bmc_s_he.py

---
+++ bmc_s_he.py
@@ -0,0 +1,8 @@
+import cxfit
+
+ft = 'ice_filter_transmission.txt'
+fn = 'flux_filt_ice_corrected_S_He.txt'
+ns1 = [7,8,9]
+ns2 = [6,7,8]
+js = [0, 1, 3]
+z=cxfit.fit_spec(fn, 16, [1, 2, 2], [ns1,ns2,ns2], js, [1.8], 0.001,0,wsig=[(1.7,1.9)], sav=['zs_he.pkl',500])
