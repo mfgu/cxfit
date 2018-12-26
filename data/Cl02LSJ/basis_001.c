bba352855fa256b76c94dcae82bd12548a9bb61b
M. F. Gu <mfgu@ssl.berkeley.edu>
2018-12-26 12:46:57 -0500

    add jj2lsj data files

---
+++ data/Cl02LSJ/basis_001.c
@@ -0,0 +1,8 @@
+Core subshells:
+
+Peel subshells:
+  1s 
+CSF(s):
+  1s ( 1)
+      1/2
+       1/2+
---
+++ data/O03LSJ/basis_001.c
@@ -0,0 +1,8 @@
+Core subshells:
+
+Peel subshells:
+  1s 
+CSF(s):
+  1s ( 2)
+         
+         0+
